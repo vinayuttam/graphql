cea2b05ede598b8267da0793572a6e1ab2298e3e
Vinay Uttam Vemparala <vinayuttamvemparala@live.com>
2017-10-21 16:33:13 +0530

    Handle undefined routes

---
+++ client/src/containers/Home.container.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const HomeContainer = () => <h1>React, Redux, React Apollo Client</h1>;
+
+export default HomeContainer;
--- client/src/containers/Root.container.js
+++ client/src/containers/Root.container.js
@@ -2,12 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { ApolloProvider } from 'react-apollo';
 import { ConnectedRouter } from 'react-router-redux';
+import { Route, Switch } from 'react-router-dom';
+
+import HomeContainer from './Home.container';
+
+const RouteNotFound = ({ location }) => (
+  <div className="route-not-found">
+    <h2>404 - Page Not Found</h2>
+  </div>
+);
 
 const RootContainer = ({ store, history, apolloClient }) => (
   <ApolloProvider client={apolloClient} store={store}>
     <ConnectedRouter history={history}>
       <div>
-        <h1>React, Redux, React Apollo Client</h1>
+        <Switch>
+          <Route exact path="/" component={HomeContainer}/>
+          <Route component={RouteNotFound} />
+        </Switch>
       </div>
     </ConnectedRouter>
   </ApolloProvider>
